004a487bf5df5364d234e3608b61a58809d8ad06
Myron Holmes <1dequanh@gmail.com>
2023-07-10 11:20:35 -0400

    First commit

---
+++ starter/src/main/java/com/devmountain/noteApp/entities/Note.java
@@ -0,0 +1,24 @@
+package com.devmountain.noteApp.entities;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "notes")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Note {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(columnDefinition = "text")
+    private String body;
+    @ManyToOne
+    @JsonBackReference
+    private User user;
+}
---
+++ starter/src/main/java/com/devmountain/noteApp/entities/User.java
@@ -0,0 +1,29 @@
+package com.devmountain.noteApp.entities;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "users")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(unique = true)
+    private String username;
+    @Column
+    private String password;
+    @OneToMany(mappedBy = "user", fetch = FetchType.LAZY,
+    cascade = {CascadeType.MERGE, CascadeType.PERSIST})
+    @JsonManagedReference
+    private Set<Note> noteSet = new HashSet<>();
+}
